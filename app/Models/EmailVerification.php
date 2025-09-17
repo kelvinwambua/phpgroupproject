a1381201ca28ef837bcba66d9e7f01f518f17b2a
kelvinwambua <shwiftykelvin@gmail.com>
2025-09-17 09:48:50 +0300

    Email Verification Models

---
+++ app/Models/EmailVerification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailVerification extends Model
+{
+    protected $fillable = [
+        'code',
+        'user_id',
+        'expire_date',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'expire_date' => 'datetime',
+        ];
+    }
+    protected function isExpired(): bool
+    {
+        return $this->expire_date->isPast();
+    }
+    protected function getNonExpiredVerificationCodes($userId)
+    {
+        return self::where('user_id', $userId)
+            ->where('expire_date', '>', now())
+            ->get();
+    }
+
+
+ 
+}
